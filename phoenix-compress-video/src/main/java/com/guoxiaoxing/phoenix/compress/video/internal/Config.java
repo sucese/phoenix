fe88ab17dfbd69a4b3e72f3516ef208b4ccf1ebf
guoxiaoxing <guoxiaoxingse@gmail.com>
2017-10-27 17:21:46 +0800

    test: add travis ci

---
+++ phoenix-compress-video/src/main/java/com/guoxiaoxing/phoenix/compress/video/internal/Config.java
@@ -0,0 +1,19 @@
+package com.guoxiaoxing.phoenix.compress.video.internal;
+
+public class Config {
+
+    /**
+     * Application root directory. All media files wi'll be stored here.
+     */
+    //public static final String VIDEO_COMPRESSOR_APPLICATION_DIR_NAME = "VideoCompressor";
+
+    /**
+     * Application folder for video files
+     */
+    //public static final String VIDEO_COMPRESSOR_COMPRESSED_VIDEOS_DIR = "/CompressedVideos/";
+
+    /**
+     * Application folder for video files
+     */
+    public static final String VIDEO_COMPRESSOR_TEMP_DIR = "/Temp/";
+}
---
+++ phoenix-compress-video/src/main/java/com/guoxiaoxing/phoenix/compress/video/internal/Sample.java
@@ -0,0 +1,19 @@
+package com.guoxiaoxing.phoenix.compress.video.internal;
+
+public class Sample {
+    private long offset = 0;
+    private long size = 0;
+
+    public Sample(long offset, long size) {
+        this.offset = offset;
+        this.size = size;
+    }
+
+    public long getOffset() {
+        return offset;
+    }
+
+    public long getSize() {
+        return size;
+    }
+}
